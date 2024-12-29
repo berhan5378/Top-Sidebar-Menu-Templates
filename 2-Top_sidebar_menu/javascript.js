a10200660cb1355d3674cfa924b1d97f71a2a816
berhan <berhan5378@gmail.com>
2024-12-29 11:32:06 +0300

    js

--- 2-Top_sidebar_menu/javascript.js
+++ 2-Top_sidebar_menu/javascript.js
@@ -1,8 +1,7 @@
-const arrow=document.querySelector('.side-bar .bxs-chevron-right-circle'), 
-      side_bar_bottom_span=document.querySelector('.side-bar .side-bar-bottom .Themes span');
+const arrow=document.querySelector('.side-bar .bxs-chevron-right-circle');
 
 arrow.addEventListener('click', () => { 
     arrow.classList.toggle('active_right_circle');
     document.querySelector('.side-bar').classList.toggle('active_side-bar');
-    (side_bar_bottom_span.style.display=='none')?side_bar_bottom_span.style.display="flex":side_bar_bottom_span.style.display="none";
+    document.querySelector('.side-bar .side-bar-bottom .Themes span').classList.toggle('translateXMinus200');
   });
