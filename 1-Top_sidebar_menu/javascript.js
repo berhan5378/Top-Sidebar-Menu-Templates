ca9ae0eb10bfd06f1a196cae7439d27ba2b9b7a4
berhan <berhan5378@gmail.com>
2025-01-22 20:40:21 +0300

    js

--- 1-Top_sidebar_menu/javascript.js
+++ 1-Top_sidebar_menu/javascript.js
@@ -1,1 +1,10 @@
- 
+const arrow=document.querySelector('.side-bar .ri-arrow-right-circle-fill'),
+      checkbox = document.querySelector('input[type="checkbox"]');
+
+      arrow.addEventListener('click', () => {
+        document.querySelector('.side-bar').classList.toggle('active_side-bar'); 
+      });
+      checkbox.addEventListener('change', () => 
+        document.querySelector('body').classList.toggle('themes')
+      );
+      
